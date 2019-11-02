112a3effabad8514052365b4bb0b5f88480a00f1
bederbourahmah <bederbourahmah@gmail.com>
2019-11-02 11:06:38 -0700

    Updates

---
+++ precache-manifest.ffb49f3e4ae8a0014b414fcc996d8790.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7939b9f5ff713423df99272361be91",
+    "url": "/WebResume/index.html"
+  },
+  {
+    "revision": "db9b2f509e3cb68e6eee",
+    "url": "/WebResume/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "5edeb4b41032b4409b2e",
+    "url": "/WebResume/static/css/main.505036be.chunk.css"
+  },
+  {
+    "revision": "db9b2f509e3cb68e6eee",
+    "url": "/WebResume/static/js/2.16f4c57d.chunk.js"
+  },
+  {
+    "revision": "5edeb4b41032b4409b2e",
+    "url": "/WebResume/static/js/main.b27736d4.chunk.js"
+  },
+  {
+    "revision": "73bcb3f6a11957080a7e",
+    "url": "/WebResume/static/js/runtime-main.3a84822a.js"
+  },
+  {
+    "revision": "dd520a83eb1113a8b26674ac2e5a3538",
+    "url": "/WebResume/static/media/Cover Letter of Beder Bourahmah.dd520a83.pdf"
+  },
+  {
+    "revision": "5f80aed6d003aeafa2e1b76e7e91c40c",
+    "url": "/WebResume/static/media/Resume of Beder Bourahmah.5f80aed6.pdf"
+  }
+]);
