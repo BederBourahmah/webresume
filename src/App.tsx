6dadfb191e998edd4aff7af742a077dc0c66420d
bederbourahmah <bederbourahmah@gmail.com>
2019-11-02 10:11:47 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Navbar, NavbarBrand, Nav, NavLink } from 'reactstrap';
+import Resume from "./downloadables/Resume of Beder Bourahmah.pdf";
+import logo from './logo.svg';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Navbar className="App-navbar">
+      <NavbarBrand href="/" className="App-link">Beder's Web Resume</NavbarBrand>
+      <Nav>
+        <NavLink href={Resume} target='_blank' className="App-link">PDF Resume</NavLink>
+      </Nav>
+    </Navbar>
+    </>
+  );
+}
+
+export default App;
