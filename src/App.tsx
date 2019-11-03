d9b11f8ba2dafdcfb8d2d5c9e862b97665decfec
bederbourahmah <bederbourahmah@gmail.com>
2019-11-03 10:20:34 -0800

    consolidating download links and adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import Resume from "./downloadables/Resume of Beder Bourahmah.pdf";
 import CoverLetter from "./downloadables/Cover Letter of Beder Bourahmah.pdf";
 import './App.css';
@@ -9,13 +9,20 @@ const App: React.FC = () => {
   return (
     <>
     <Navbar className="App-navbar">
-      <NavbarBrand href={process.env.PUBLIC_URL} className="App-link">Beder's Web Resume</NavbarBrand>
-      <Nav>
-        <NavLink href={Resume} target='_blank' className="App-link">PDF Resume</NavLink>
-      </Nav>
-      <Nav>
-        <NavLink href={CoverLetter} target='_blank' className="App-link">PDF Cover Letter</NavLink>
-      </Nav>
+      <NavbarBrand href={process.env.PUBLIC_URL} className="App-navLinks">Beder's Web Resume</NavbarBrand>
+      <UncontrolledDropdown inNavbar>
+        <DropdownToggle nav caret className="App-navLinks">
+          Downloads
+        </DropdownToggle>
+        <DropdownMenu right className="App-dropdownMenu">
+          <DropdownItem href={Resume} target='_blank' className="App-navLinks">
+            PDF Resume
+          </DropdownItem >
+          <DropdownItem href={CoverLetter} target='_blank' className="App-navLinks">
+            PDF Cover Letter
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
     </Navbar>
     </>
   );
