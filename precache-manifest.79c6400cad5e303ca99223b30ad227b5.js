5e90a16b7762b6d055d04308294b5cb143602e4b
bederbourahmah <bederbourahmah@gmail.com>
2019-11-04 13:20:19 -0800

    Updates

--- precache-manifest.79c6400cad5e303ca99223b30ad227b5.js
+++ precache-manifest.79c6400cad5e303ca99223b30ad227b5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a4515d3ff3c652b4ed4f3003f4c6bfb",
+    "revision": "4d05dae349112c53f72f7db2820e41cb",
     "url": "/webresume/index.html"
   },
   {
-    "revision": "db9b2f509e3cb68e6eee",
+    "revision": "dd40b2f05d37de671c0d",
     "url": "/webresume/static/css/2.833dd627.chunk.css"
   },
   {
-    "revision": "052b797b96e6e9cbbc19",
-    "url": "/webresume/static/css/main.4dfd6aff.chunk.css"
+    "revision": "875102f2a996be524038",
+    "url": "/webresume/static/css/main.925e3f7f.chunk.css"
   },
   {
-    "revision": "db9b2f509e3cb68e6eee",
-    "url": "/webresume/static/js/2.16f4c57d.chunk.js"
+    "revision": "dd40b2f05d37de671c0d",
+    "url": "/webresume/static/js/2.bd5f1798.chunk.js"
   },
   {
-    "revision": "052b797b96e6e9cbbc19",
-    "url": "/webresume/static/js/main.63e4ac27.chunk.js"
+    "revision": "875102f2a996be524038",
+    "url": "/webresume/static/js/main.2cfb7450.chunk.js"
   },
   {
     "revision": "37455ac12ed089fc41c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webresume/precache-manifest.b3aab454127ee90007c640dd5f4c4ff7.js"
+  "/webresume/precache-manifest.79c6400cad5e303ca99223b30ad227b5.js"
 );
 
 self.addEventListener('message', (event) => {
