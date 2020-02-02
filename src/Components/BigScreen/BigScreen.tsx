4baeaf5d695e254110dfd1454d06e62af7d614fc
bederbourahmah <bederbourahmah@gmail.com>
2020-02-01 17:54:12 -0800

    multiple improvements to layout and content

--- src/Components/BigScreen/BigScreen.tsx
+++ src/Components/BigScreen/BigScreen.tsx
@@ -1,46 +1,63 @@
 import React from 'react';
 import { Navbar, NavbarBrand, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink, Nav } from 'reactstrap';
-import Resume from "../../downloadables/Resume of Beder Bourahmah.pdf";
-import CoverLetter from "../../downloadables/Cover Letter of Beder Bourahmah.pdf";
 import './BigScreen.css';
 import { Link, Switch, Route, HashRouter } from "react-router-dom";
-import Education from '../Education/Education';
+import Education from "../Education/Education";
 import Career from '../Career/Career';
 
 export default class BigScreen extends React.Component {
   render() {
     return (
       <HashRouter>
+        
         <Navbar className="App-navbar">
-        <Link className="App-navLinks" to="/"><NavbarBrand >Beder's Web Resume</NavbarBrand></Link>
-        <Nav className="NavigationBar-items" >
-          <Link className="App-navLinks" to="/education"><NavLink>Education</NavLink></Link>
-          <Link className="App-navLinks" to="/career"><NavLink>Career</NavLink></Link>
-          <UncontrolledDropdown inNavbar>
-            <DropdownToggle nav caret className="App-navLinks">
-              Downloads
-            </DropdownToggle>
-            <DropdownMenu right className="App-dropdownMenu">
-              <DropdownItem href={Resume} target='_blank' className="App-navLinks">
-                Resume
-              </DropdownItem >
-              <DropdownItem href={CoverLetter} target='_blank' className="App-navLinks">
-                Cover Letter
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </Navbar>
+          <Link className="App-navLinks navBrand" to="/">Beder's Web Resume</Link>
+          <Nav className="NavigationBar-items">
+            <Link className="App-navLinks" to="/education"><NavLink>Education</NavLink></Link>
+            <Link className="App-navLinks" to="/career"><NavLink>Career</NavLink></Link>
+            <UncontrolledDropdown inNavbar>
+              <DropdownToggle nav caret className="App-navLinks">
+                Downloads
+              </DropdownToggle>
+              <DropdownMenu right className="App-dropdownMenu">
+                <DropdownItem className="App-navLinks">
+                  <Link className="App-navLinks" to="/resume">
+                    <NavLink>Resume</NavLink>
+                  </Link>
+                </DropdownItem>
+                <DropdownItem href={"#"} target='_blank' className="App-navLinks">
+                <Link className="App-navLinks" to="/cover_letter">
+                    <NavLink>Cover Letter</NavLink>
+                  </Link>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Navbar>
 
-      <Switch>
-        <Route path="/education">
-          <Education />
-        </Route>
-        <Route path="/career">
-          <Career />
-        </Route>
-      </Switch>
-    </HashRouter>
+        <Switch>
+          <div className="main">
+            <Route path="/education">
+              <Education />
+            </Route>
+            <Route path="/career">
+              <Career />
+            </Route>
+            <Route path="/resume">
+              <iframe
+                src="https://onedrive.live.com/embed?cid=3170DAEDCAD61BF4&resid=3170DAEDCAD61BF4%214244&authkey=AHEOa37xX3ovqXA&em=2"
+                className="oneDriveDoc"
+              />
+            </Route>
+            <Route path="/cover_letter">
+            <iframe
+              src="https://onedrive.live.com/embed?cid=3170DAEDCAD61BF4&resid=3170DAEDCAD61BF4%214246&authkey=AH_hdkvTb38JJgQ&em=2"
+              className="oneDriveDoc"
+              ></iframe>
+            </Route>
+          </div>
+        </Switch>
+      </HashRouter>
     );
   }
 }
--- src/Components/Education/Education.tsx
+++ src/Components/Education/Education.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Col, Row } from 'reactstrap';
 import './Education.css';
+import MdcLogo from "./miami-dade-college.png"
+import FiuLogo from "./Florida_International_University_logo.svg"
 
 export default class Education extends React.Component {
   render() {
     return (
-      <Col>
-        <Row className="Education-header">
-          Miami Dade College
-        </Row>
-        <Row className="Education-body">
-          I attended Miami Dade College from the Fall Semester 2008 to the Summer Semester 2010. While enrolled at MDC I earned 63 credits with a cumulative GPA of 3.68.
-        </Row>
-        <Row className="Education-header">
-          Florida International University
-        </Row>
-        <Row className="Education-body">
-          I attended Florida International University from the Fall Semester 2010 to the Spring Semester 2013. While enrolled at FIU I earned 95 credits with a cumulative GPA of 3.27.
-          I graduated in April, 2013, receiving my Bachelor of Science in Electrical Engineering degree.
-        </Row>
-      </Col>
+      <div className="educationMainBox">
+
+        <div className="educationBox">
+          <img className="logo" src={MdcLogo} alt="Miami Dade College Logo"/>  
+          <p className="Education-body">
+            I attended Miami Dade College from the Fall Semester 2008 to the Summer Semester 2010. While enrolled at MDC I earned 63 credits with a cumulative GPA of 3.68.
+          </p>
+        </div>
         
+        <div className="educationBox">
+          <img className="logo" src={FiuLogo} alt="Florida International University Logo"/>
+          <p className="Education-body">
+            I attended Florida International University from the Fall Semester 2010 to the Spring Semester 2013. While enrolled at FIU I earned 95 credits with a cumulative GPA of 3.27.
+            I graduated in April, 2013, receiving my Bachelor of Science in Electrical Engineering degree.
+          </p>
+        </div>
+        
+      </div>
     );
   }
 }
